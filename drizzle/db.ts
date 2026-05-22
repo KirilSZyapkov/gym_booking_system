1dafff709fec1b3103452ef6e23b396645ea9e4d
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-05-23 00:13:55 +0300

    inital commit

---
+++ drizzle/db.ts
@@ -0,0 +1,8 @@
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema";
+
+const sql = new Pool({connectionString: process.env.DATABASE_URL!});
+const db = drizzle(sql, {schema});
+
+export default db;
