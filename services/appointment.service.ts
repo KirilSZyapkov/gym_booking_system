f0d9f79cf5f8df59b4c0383e9323de944199aaa7
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-06-14 22:09:27 +0300

    Add appointment service functions for managing appointments

---
+++ services/appointment.service.ts
@@ -0,0 +1,30 @@
+import db from "@/drizzle/db";
+import { appointment } from "@/drizzle/schemas/appointment-schema";
+import { eq } from "drizzle-orm";
+
+type Params = {
+  clientId: string,
+  trainer: string,
+  startTime: Date,
+  endTime: Date,
+}
+
+export async function createNewAppointmentService(data: Params) {
+
+}
+
+export async function getAppointmentByIdService(id: string) {
+  
+}
+
+export async function getAllAppointmentsService() {
+  
+}
+
+export async function updateAppointmentService(id: string, data: Partial<typeof appointment.$inferSelect>) {
+  
+}
+
+export async function cancelAppointmentByIdService(id: string) {
+  
+}
