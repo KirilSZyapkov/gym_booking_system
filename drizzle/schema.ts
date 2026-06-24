04cd1583d19b22f5f3f41c86df771f97de931e9a
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-06-24 20:44:06 +0300

    Update schema exports to include trainer and trainerSchedule schemas

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,3 +1,5 @@
 export * from "./schemas/auth-schema";
 export * from "./schemas/appointment-schema";
 export * from "./schemas/client-schema";
+export * from "./schemas/trainer-schema";
+export * from "./schemas/trainerSchedule-schema";
